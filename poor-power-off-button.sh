4352f08ed3c5b690f1c012e5e436943f45f970f7
karpoke <karpoke+github@gmail.com>
2019-12-29 14:25:29 +0100

    Added script to make an action when USB is connected

---
+++ poor-power-off-button.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+
+# shutdown raspberry pi safely when connecting the correct
+# poor-power-off-button USB
+
+# lsbusb:
+# Bus 001 Device 006: ID 0950:2544 Kingston DataTraveler 2.0 Stick (2GB)
+# 0930: vendor ID
+# 6544: product ID
+
+# add rule:
+# cat /etc/udev/rules.d/99-poor-power-off-button.rules
+# ATTRS{idVendor}=="0950", ATTRS{idProduct}=="2544", ACTION=="add", RUN+="/root/poor-power-off-button.sh"
+
+# to see all environment variables available:
+# LOGFILE="/tmp/poor-power-off-button.log"
+# env >> "$LOGFILE"
+
+logger "Poor power off USB detected. Shutting down..."
+/sbin/shutdown -h now
