66abf2ffbb96761d9fc9590e4b13a82e5749ee4f
karpoke <karpoke+github@gmail.com>
2020-01-06 03:27:34 +0100

    Use 'xargs -n1' instead of for loop

--- fail2ban-client-status-all.sh
+++ fail2ban-client-status-all.sh
@@ -1,7 +1,4 @@
 #!/bin/bash
 
 # https://gist.github.com/kamermans/1076290
-JAILS=$(/usr/bin/fail2ban-client status | tail -1 | awk -F: '{print $2}' | sed 's/,//g')
-for JAIL in $JAILS; do
-    /usr/bin/fail2ban-client status "$JAIL"
-done
+/usr/bin/fail2ban-client status | tail -1 | awk -F: '{print $2}' | sed 's/,//g' | xargs -n1 fail2ban-client status
