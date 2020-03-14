ff94c7f54be7cf7d24e47f540179977eb95cc8c4
karpoke <karpoke+github@gmail.com>
2020-03-14 11:31:36 +0100

    Fix command not found in download by keywords script

--- add-torrent-by-keywords.sh
+++ add-torrent-by-keywords.sh
@@ -2,13 +2,24 @@
 
 # downloads the first torrent found in 1337x by using the given keywords
 
-FILE=$(mktmp)
+FILE=$(mktemp)
 DOMAIN="https://1337x.to"
 KW=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$*")
 URL="$DOMAIN/search/$KW/1/"
-wget -Ufirefox "$URL" -O "$FILE"
-PATH=$(grep -Eo '/torrent/[^"]+' "$FILE" | head -1)
-URL="$DOMAIN/$PATH"
-wget -Ufirefox "$URL" -O "$FILE"
+wget -Ufirefox "$URL" -O "$FILE" 2>/dev/null
+
+URLPATH=$(grep -Eo 'torrent/[^"]+' "$FILE" | head -1)
+if [ -z "$URLPATH" ]; then
+    echo "Torrent not found"
+    exit 1
+fi
+
+URL="$DOMAIN/$URLPATH"
+wget -Ufirefox "$URL" -O "$FILE" 2>/dev/null
 MAGNET=$(grep -Eo 'magnet:\?[^"]+' "$FILE")
-transmission-remote --add "$MAGNET"
+if [ -z "$MAGNET" ]; then
+    echo "Magnet not found"
+    exit 1
+fi
+
+transmission-remote --add "$MAGNET" >/dev/null && echo "Ok" || echo "Error"
