a8e0f22f781f8642e3b6b48025295944d5b01d86
karpoke <karpoke+github@gmail.com>
2020-04-11 21:37:32 +0200

    Added brute force 1to50 version

--- 1to50/1to50.sh
+++ 1to50/1to50.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+
 # http://zzzscore.com/1to50/en/result/#!v=U2FsdGVkX18KIBpeCrPiZQN59U8%3D
 
 # make number images the same size 60x60
@@ -8,38 +9,73 @@
 
 # set -x
 
-click_image () {
-    COORDS=$(visgrep table.png "$1" | awk '{print $1}')
-    # shellcheck disable=SC2039
+get_coords () {
+    T=${3-0}
+    COORDS=$(visgrep -t "$T" "$1" "$2" | awk '{print $1}')
     X=$(echo "$COORDS" | awk -F, '{print $1}')
     Y=$(echo "$COORDS" | awk -F, '{print $2}')
+}
+
+click_image () {
+    set -x
+    get_coords table.png "$1"
+
     if [ -z "$X" ]; then
         echo "$i not found. trying harder..."
-        COORDS=$(visgrep -t 10000 table.png "$1" | awk '{print $1}')
-        # shellcheck disable=SC2039
-        X=$(echo "$COORDS" | awk -F, '{print $1}')
-        Y=$(echo "$COORDS" | awk -F, '{print $2}')
+        get_coords table.png "$1" 10000
     fi
     [ -z "$X" ] && echo "$i not found" && exit 1
     xdotool mousemove "$((OFFSET_X + X + 15))" "$((OFFSET_Y + Y + 15))"
-    xdotool click 1  # left button
+    xdotool click --delay 0 1  # left button
 }
 
-scrot screen.png
+get_first_table () {
+    scrot screen.png
 
-COORDS=$(visgrep -t1000 screen.png detect.png | awk '{print $1}')
-# shellcheck disable=SC2039
-OFFSET_X=$(echo "$COORDS" | awk -F, '{print $1}')
-OFFSET_Y=$(echo "$COORDS" | awk -F, '{print $2}')
-convert screen.png -crop "420x580+${OFFSET_X}+${OFFSET_Y}" table.png
+    get_coords screen.png detect.png 10000
+    OFFSET_X=$((X-30))
+    OFFSET_Y=$Y
+    convert screen.png -crop "420x580+${OFFSET_X}+${OFFSET_Y}" table.png
+}
 
-for i in $(seq 1 1 25); do
-    click_image  "$i.png"
-done
+get_second_table () {
+    scrot -d 1.5 screen.png
+    convert screen.png -crop "420x580+${OFFSET_X}+${OFFSET_Y}" table.png
+}
 
-scrot -d 1.5 screen.png
-convert screen.png -crop "420x580+${OFFSET_X}+${OFFSET_Y}" table.png
+v1 () {
+    # visual grep each number
+    get_first_table
 
-for i in $(seq 26 1 50); do
-    click_image  "$i.png"
-done
+    for i in $(seq 1 1 25); do
+        click_image  "$i.png"
+    done
+
+    get_second_table
+
+    for i in $(seq 26 1 50); do
+        click_image  "$i.png"
+    done
+}
+
+v2 () {
+    # brute force each number
+    get_first_table
+
+    for _ in $(seq 1 1 50); do
+        for i in $(seq 0 1 4); do
+            for j in $(seq 0 1 4); do
+                TX=$((OFFSET_X + i*85))
+                TY=$((OFFSET_Y + j*85 + 185))
+                xdotool mousemove "$TX" "$TY"
+                xdotool click --delay 0 1  # left button
+            done
+        done
+    done
+}
+
+main () {
+    v2
+}
+main
+
