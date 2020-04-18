bcedd0fc36edd53abd3822398bf8a97c34b546e5
karpoke <karpoke+github@gmail.com>
2020-04-18 17:49:15 +0200

    Allow files to be included in Telegram notifications

--- notify-by-telegram.sh
+++ notify-by-telegram.sh
@@ -1,39 +1,90 @@
 #!/bin/bash
 # https://github.com/python-telegram-bot/python-telegram-bot/wiki/Performance-Optimizations#server-location
 
-CREDSFILE="$HOME/.tg_auth"
+# Credentials file: ~/.tg_auth
+# $ cat ~/.tg_auth
+# TOKEN=550e8400-e29b-41d4-a716-446655440000
+# ID=123456
 
-usage () {
-    echo "ERROR: $CREDSFILE must contain:"
-    echo ""
-    echo "TOKEN=550e8400-e29b-41d4-a716-446655440000"
-    echo "ID=123456"
+get_credentials () {
+    CREDSFILE="$HOME/.tg_auth"
+
+    if [ ! -r "$CREDSFILE" ]; then
+        logger -t "$(basename "$0")" "Credentials file not found"
+        exit 1
+    fi
+
+    # shellcheck disable=SC1090
+    # change this for: grep -cx TOKEN=(.*)
+    source "$CREDSFILE"
+
+    if [ -z "$TOKEN" ] || [ -z "$ID" ]; then
+        logger -t "$(basename "$0")" "Credentials not valid"
+        exit 1
+    fi
 }
 
-if [ ! -r "$CREDSFILE" ]; then
-    usage
-    exit 1
-fi
+get_file_endpoint () {
+    if file "$1" | grep -qiE 'image|bitmap'; then
+        ENDPOINT="sendPhoto"
+    elif file "$1" | grep -qi 'audio'; then
+        ENDPOINT="sendAudio"
+    else
+        ENDPOINT="sendDocument"
+    fi
+    echo $ENDPOINT
+}
 
-# shellcheck disable=SC1090
-source "$CREDSFILE"
+get_endpoint () {
+    if test -r "$1"; then
+        ENDPOINT=$(get_file_endpoint "$1")
+    else
+        ENDPOINT="sendMessage"
+    fi
 
-if [ -z "$TOKEN" ] || [ -z "$ID" ]; then
-    usage
-    exit 1
-fi
+    API_URL="https://api.telegram.org/bot$TOKEN/$ENDPOINT"
+    echo "$API_URL"
+}
 
-if [ "$#" -eq 0 ]; then
-    echo "$0 <message>"
-    exit 1
-fi
+get_file_data () {
+    DATA=""
+    if test -r "$1"; then
+        REAL_PATH="$(readlink -f "$1")"
+        if file "$1" | grep -qiE 'image|bitmap'; then
+            FIELD="photo"
+        elif file "$1" | grep -qi 'audio'; then
+            FIELD="audio"
+        else
+            FIELD="document"
+        fi
+        DATA="$FIELD=@$REAL_PATH"
+    fi
+    echo "$DATA"
+}
 
-API_URL="https://api.telegram.org/bot$TOKEN/sendMessage"
+main () {
+    if [ "$#" -eq 0 ]; then
+        echo "$(basename "$0") [<file>] <message>"
+        exit 1
+    fi
 
-/usr/bin/curl \
-    -s \
-    -X POST \
-    -H "Content-Type: application/json" \
-    -d "{\"chat_id\": \"$ID\", \"text\": \"$*\", \"disable_notification\": true}" \
-    -o /dev/null \
-    "$API_URL"
+    get_credentials
+
+    ENDPOINT="$(get_endpoint "$1")"
+    FILEDATA="$(get_file_data "$@")"
+
+    if [ -n "$FILEDATA" ]; then
+        shift
+        /usr/bin/curl "$ENDPOINT" -vvvv -s -o /dev/null \
+            -F "chat_id=$ID" \
+            -F "caption=$*" \
+            -F "$FILEDATA"
+   else
+        /usr/bin/curl "$ENDPOINT" -vvvv -s -o /dev/null \
+            -d "chat_id=$ID" \
+            -d "text=$*"
+   fi
+}
+
+main "$@"
+
