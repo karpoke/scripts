71f18ee2b5edce89a547ab3efe3e90f5b2a4a7b9
karpoke <karpoke+github@gmail.com>
2020-04-26 17:58:22 +0200

    Avoid unnecessary conversion from torrent to hash before adding it to transmission

--- add-torrent-by-keywords.sh
+++ add-torrent-by-keywords.sh
@@ -1,24 +1,30 @@
 #!/bin/bash
 
-# downloads the first torrent found in 1337x by using the given keywords
+# downloads the first torrent found in 1337x given some keywords
 
-FILE=$(mktemp)
+# torrent's page
+
 DOMAIN="https://1337x.to"
 KW=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$*")
 URL="$DOMAIN/search/$KW/1/"
-wget -Ufirefox "$URL" -O "$FILE" 2>/dev/null
+TEMPFILE="/tmp/$KW.13337x.tmp"
+wget -q -Ufirefox "$URL" -O "$TEMPFILE"
 
-URLPATH=$(grep -Eo 'torrent/[^"]+' "$FILE" | head -1)
+# torrent file
+
+URLPATH=$(grep -Eo 'torrent/[^"]+' "$TEMPFILE" | head -1)
 if [ -z "$URLPATH" ]; then
     logger -t "$(basename "$0")" "Torrent not found"
     exit 1
 fi
-
 URL="$DOMAIN/$URLPATH"
-wget -Ufirefox "$URL" -O "$FILE" 2>/dev/null
-MAGNET=$(grep -Eo 'magnet:\?[^"]+' "$FILE")
-add-torrent-by-magnet.sh "$MAGNET"
+TORRENT="/tmp/$KW.torrent"
+wget -q -Ufirefox "$URL" -O "$TORRENT"
 
-FILENAME="${URL//*\//}"
-notify-by-telegram.sh "$FILENAME" "$KW"
+
+transmission-remote --add "$TEMPFILE" >/dev/null && echo "Ok" || echo "Error"
+transmission-add-trackers.sh
+notify-by-telegram.sh "$TORRENT" "$KW"
+
+rm -f "$TEMPFILE" "$TORRENT"
 
