396129f0ec6ef012af0cef48d4af4e933acd7d3d
karpoke <karpoke+github@gmail.com>
2020-04-30 00:07:21 +0200

    Move scripts to one-liner functions in bash aliases

--- fail2ban-client-status-all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-# https://gist.github.com/kamermans/1076290
-fail2ban-client status | tail -1 | awk -F: '{print $2}' | sed 's/,//g' | xargs -n1 fail2ban-client status
-
--- uri_encode.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-# perl do not convert ( and ) to %28 and %29
-# perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$*"
-python -c "from __future__ import print_function; import urllib; print(urllib.quote('''$*'''), end='')"
-
