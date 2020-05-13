833eed2224930b18bf3bfdda2b7894ca09beb535
karpoke <karpoke+github@gmail.com>
2020-05-13 12:22:39 +0200

    Check for root user before running lynis

--- lynis.sh
+++ lynis.sh
@@ -2,6 +2,8 @@
 
 [ -z "$1" ] && echo "Usage: $(basename "$0") rcpto@example.com" && exit 1
 
+[ "$(id -u)" -ne 0 ] && echo "ERROR: You must be root user to run this program" && exit 3
+
 OUTFILE=$(mktemp)
 /usr/bin/nice -n 0 /usr/sbin/lynis --cronjob --auditor "$(hostname -f)" > "$OUTFILE"
 
