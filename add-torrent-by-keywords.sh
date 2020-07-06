d120ab1ea229fb1fd02ade5a0d8d8db4d1276eb6
karpoke <karpoke+github@gmail.com>
2020-07-06 23:55:00 +0200

    Look for magnet link when adding torrent by keywords
    1337x seems to have added some anti scrapping protection, so instead of
    downloading torrent file, we will use magnet link.

--- add-torrent-by-keywords.sh
+++ add-torrent-by-keywords.sh
@@ -2,7 +2,7 @@
 
 # downloads the first torrent found in 1337x given some keywords
 
-# torrent's page
+# search page
 
 DOMAIN="https://1337x.to"
 KW=$(perl -MURI::Escape -e 'print uri_escape($ARGV[0]);' "$*")
@@ -10,7 +10,7 @@ URL="$DOMAIN/search/$KW/1/"
 TEMPFILE="/tmp/$KW.13337x.tmp"
 wget -q -Ufirefox "$URL" -O "$TEMPFILE"
 
-# torrent file
+# torrent's page
 
 URLPATH=$(grep -Eo 'torrent/[^"]+' "$TEMPFILE" | head -1)
 if [ -z "$URLPATH" ]; then
@@ -18,14 +18,20 @@ if [ -z "$URLPATH" ]; then
     exit 1
 fi
 URL="$DOMAIN/$URLPATH"
-TORRENT="/tmp/$KW.torrent"
-wget -q -Ufirefox "$URL" -O "$TORRENT"
+wget -q -Ufirefox "$URL" -O "$TEMPFILE"
 
+# add magnet
+
+MAGNET=$(grep -Eo 'magnet[^"]+' "$TEMPFILE" | head -1)
 
 service transmission-daemon status >/dev/null 2>&1 || service transmission-daemon start
-transmission-remote --add "$TEMPFILE" >/dev/null && echo "Ok" || echo "Error"
-transmission-add-trackers.sh
-notify-by-telegram.sh "$TORRENT" "$KW"
 
-rm -f "$TEMPFILE" "$TORRENT"
+if transmission-remote --add "$MAGNET" >/dev/null; then
+    notify-by-telegram.sh "Found and added $KW"
+    transmission-add-trackers.sh
+else
+    logger -t "$(basename "$0")" "Error adding torrent"
+fi
+
+rm -f "$TEMPFILE"
 
