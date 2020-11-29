6a884ec99d0747f55038cbe89af7dde9762c93f5
karpoke <karpoke+github@gmail.com>
2020-11-29 23:16:25 +0100

    Prune unwanted files from downloading torrents

--- transmission-prune-torrents.sh
+++ transmission-prune-torrents.sh
@@ -2,6 +2,20 @@
 
 # avoid downloading url, nfo, txt and jpg files
 
+get_torrent_name () {
+    TORRENT_ID=$1
+    TORRENT_NAME=$(transmission-remote --torrent "$TORRENT_ID" --info | grep Name | cut -c9-)
+    echo "$TORRENT_NAME"
+}
+
+get_torrent_file_name () {
+    TORRENT_ID=$1
+    FILE_ID=$2
+    TORRENT_NAME=$(transmission-remote --torrent "$TORRENT_ID" --info-files | grep "$FILE_ID:" | awk '{for(i=7;i<=NF;++i)printf $i""FS ; print ""}')
+    echo "$TORRENT_NAME"
+}
+
+
 transmission-remote --list |
     grep -vE 'Seeding|100%' |
     sed -e '1d;$d;s/^ *//' |
@@ -14,6 +28,9 @@ transmission-remote --list |
             awk -F: '{print $1}' |
             while read -r FILE_ID; do
                 tranmission-remote -t "$TORRENT_ID" --no-get "$FILE_ID"
+		TORRENT_NAME=$(get_torrent_name "$TORRENT_ID")
+		FILE_NAME=$(get_torrent_file_name "$TORRENT_ID" "$FILE_ID")
+		logger -t "$(basename "$0")" "Torrent: $TORRENT_NAME Discard file: $FILE_NAME"
             done
     done
 
